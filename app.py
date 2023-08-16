00a5419267b1f19fa837d02e21771fc35d8c9284
JJ <127033262+Jayavathsan@users.noreply.github.com>
2023-08-16 19:00:15 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+
+from langchain.llms import OpenAI
+
+#Function to return the response
+def load_answer(question):
+    llm = OpenAI(model_name="text-davinci-003",temperature=0)
+    answer=llm(question)
+    return answer
+
+
+#App UI starts here
+st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+st.header("LangChain Demo")
+
+#Gets the user input
+def get_text():
+    input_text = st.text_input("You: ", key="input")
+    return input_text
+
+
+user_input=get_text()
+response = load_answer(user_input)
+
+submit = st.button('Generate')  
+
+#If generate button is clicked
+if submit:
+
+    st.subheader("Answer:")
+
+    st.write(response)
